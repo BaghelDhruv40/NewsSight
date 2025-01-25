95c9eeab865dcb1ab831f0a0f4f89f4c2241fc6f
BaghelDhruv40 <dhruvbaghel40@gmail.com>
2025-01-25 19:17:43 +0530

    updated

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -5,7 +5,7 @@ import NewsItem from "./NewsItem"
 export default function NewsBoard({category,country}) {
     const [articles,setArticles]=useState([])
     useEffect(()=>{
-        let url=`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        let url=`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.VITE_API_KEY}`;
         
         fetch(url)
        .then(response=>response.json()).then(data=>{setArticles(data.articles)})
